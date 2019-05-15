8e5bb9772828c62420330c3e7889c33a78ca978b
Nwobodo Kenneth <kennethgranero@gmail.com>
2019-05-15 03:19:16 +0100

    New prime file

---
+++ Prime.java
@@ -0,0 +1,16 @@
+import java.util.*
+
+public class Prime{
+
+	public static void main(String[] args){
+
+		Scanner p = new Scanner(System.in);
+
+	System.out.print("Enter value: ");
+
+		int number = p.nextInt();
+
+
+	}
+
+}
